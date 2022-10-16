60e8f70b7ab4ae58a33d0200e3c85180a826a6d7
David Mboko <98537263+Meekdavid@users.noreply.github.com>
2022-10-16 05:46:46 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,4 +1,4 @@
-Efficient efficient efficient efficient 
+Efficient efficient efficient efficient
 #!/usr/bin/python3
 # Fabfile to generates a .tgz archive from the contents of web_static.
 import os.path
