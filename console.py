e64f22aefe8c986d466014df5073c5372739ab76
David Mboko <98537263+Meekdavid@users.noreply.github.com>
2023-05-06 14:25:27 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Defines entry point of the command interpreter."""
-import cmd effective eff
+import cmd effective ef
 import re
 from shlex import split
 from models import storage
